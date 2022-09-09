952801c0209839e599f5d7eb24ec148a50bbdd0b
Danil Prisiazhnyi <judas.priest999@gmail.com>
2022-09-09 20:58:28 +0400

    added docs for CloudFunctionRequestHelper

--- utils/CloudFunctionRequestHelper.go
+++ utils/CloudFunctionRequestHelper.go
@@ -9,11 +9,16 @@ import (
 	crun "github.com/pip-services3-gox/pip-services3-commons-gox/run"
 )
 
+// Helper struct that allow prepare of requests data
 var CloudFunctionRequestHelper = _TCloudFunctionRequestHelper{}
 
 type _TCloudFunctionRequestHelper struct {
 }
 
+// Returns correlationId from request struct
+// Parameters:
+//		- req	request struct
+// Returns correlation id string or empty
 func (c *_TCloudFunctionRequestHelper) GetCorrelationId(req *http.Request) string {
 	correlationId := req.URL.Query().Get("correlation_id")
 	if correlationId == "" {
@@ -22,6 +27,10 @@ func (c *_TCloudFunctionRequestHelper) GetCorrelationId(req *http.Request) strin
 	return correlationId
 }
 
+// Returns command from request struct
+// Parameters:
+//		- req	request struct
+// Returns command string or empty
 func (c *_TCloudFunctionRequestHelper) GetCommand(req *http.Request) (string, error) {
 	cmd := req.URL.Query().Get("cmd")
 
@@ -42,6 +51,11 @@ func (c *_TCloudFunctionRequestHelper) GetCommand(req *http.Request) (string, er
 	return cmd, nil
 }
 
+// Returns body of request
+// Parameters:
+//		- req	request struct
+//		- target	the target instance to which the result will be written
+// Returns error
 func (c *_TCloudFunctionRequestHelper) DecodeBody(req *http.Request, target any) error {
 	bodyBytes, err := ioutil.ReadAll(req.Body)
 
@@ -61,6 +75,10 @@ func (c *_TCloudFunctionRequestHelper) DecodeBody(req *http.Request, target any)
 	return nil
 }
 
+// Get body of request as Parameters struct
+// Parameters:
+//		- req	request struct
+// Returns Parameters
 func (c *_TCloudFunctionRequestHelper) GetParameters(req *http.Request) *crun.Parameters {
 	var params map[string]any
 
